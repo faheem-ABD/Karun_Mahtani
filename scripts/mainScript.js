f1fbd983df91eef6f6efd1d5cda0700d08990c35
faheem-ABD <faheem.abdeenkm@gmail.com>
2025-08-06 03:36:13 +0200

    changes

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,5 +1,6 @@
-window.addEventListener('DOMContentLoaded', () => {
-  requestAnimationFrame(() => {
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    document.body.classList.remove('opacity-0');
     document.body.classList.add('opacity-100');
-  });
+  }, 50); // tiny delay for smoothness
 });
